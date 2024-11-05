1b8003218cfca4635b2e9d1bd59f6b1dc0a2d5f3
shivanshagarwal10 <shivanshagrawal21@gmail.com>
2024-11-05 11:27:21 +0530

    all changes

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h2>Welcome to the Home Page</h2>
+      <p>This is a protected page accessible after login.</p>
+      {/* Add carousel or image cards here */}
+    </div>
+  );
+}
+
+export default Home;
